22039a22cf0e98151246c32e3644e7146d339034
Joseph Green <jp3green@uwaterloo.ca>
2022-05-05 14:37:42 -0400

    remove mongodb url

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 
-const uri = "mongodb+srv://etakiuser:[pwd]@cluster0.ot16m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = "[mongourl]";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect();
 
