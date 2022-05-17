cfd33f7c6f6e66bb7d250e10ec59d7fe5b7be7c9
Joseph Green <jp3green@uwaterloo.ca>
2022-05-17 13:19:31 -0400

    move head elements to _app

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,12 @@
 import './_app.css';
+import Head from 'next/head';
 
-
-function BaseApp({ Component, pageProps}) {
-  return <Component {...pageProps}/>
+export default function BaseApp({ Component, pageProps}) {
+  return [
+          <Head>
+            <link rel="shortcut icon" id='favicon' href="./favicon.ico" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <title>ETAKI</title>
+          </Head>,
+          <Component {...pageProps}/>]
 }
-export default BaseApp
--- client/pages/_document.js
+++ client/pages/_document.js
@@ -4,9 +4,7 @@ export default function Document() {
     return (
         <Html lang='en'>
             <Head>
-                <meta charset="utf-8" />
-                <link rel="icon" id='favicon' href="favicon.ico" />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta charSet="utf-8" />
                 <meta name="theme-color" content="#000000" />
                 <meta
                 name="description"
@@ -18,9 +16,8 @@ export default function Document() {
                 rel="stylesheet"
                 href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
                 integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-                crossorigin="anonymous"
+                crossOrigin="anonymous"
                 />
-                <title>ETAKI</title>
             </Head>
             <body>
                 <Main />
